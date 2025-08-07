814ff46e259fafb0117028405f91e330f1542db1
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-08-07 17:37:13 +0100

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,23 @@
+void main()
+{
+    int a = 5;
+    int b = 0;
+    int c = 1;
+    
+    if (a==b)
+        call hello;
+    while (a<5)
+        call hello;
+    for (a=0;a<10;a++)
+        call hello
+    return 2;
+}
+void hello()
+{
+   int a = 5;
+   int b = 0;
+   int c = 1;
+   if (a==0)
+      call hello;
+   return 0;
+}
